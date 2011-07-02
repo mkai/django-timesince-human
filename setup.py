d1858c089a1e0b03a4bc7da1f21203055ecb79e2
Markus Kaiserswerth <mkai@sensun.org>
2011-07-02 02:14:40 +0200

    Updated German localization

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-timesince-human',
-      version='0.1',
+      version='0.11',
       description="Humanized and localized timesince template filter for Django",
       long_description="",
       author='Markus Kaiserswerth',
