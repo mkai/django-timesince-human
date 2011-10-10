2985909067895f8bbd262da076404cfef425d667
Markus Kaiserswerth <github@sensun.org>
2011-10-11 01:16:14 +0200

    Cleaned up, documented and added TODOs

--- timesince_human/templatetags/timesince_human.py
+++ timesince_human/templatetags/timesince_human.py
@@ -1,5 +1,3 @@
-# Humanized and localized timesince template filter
-# originally from: http://djangosnippets.org/snippets/2275/
 from django import template
 from django.utils.translation import ugettext, ungettext
 import datetime
@@ -7,7 +5,14 @@ import datetime
 register = template.Library()
 
 @register.filter(name='timesince_human')
-def humanize_timesince(date):
+def humanize_timesince(date): # TODO: let user specify format strings
+    """
+    Returns a translated, humanized representation of the time delta between
+    a given date and the current date.
+    
+    Originally from: http://djangosnippets.org/snippets/2275/
+    
+    """
     delta = datetime.datetime.now() - date
 
     num_years = delta.days / 365
@@ -15,18 +20,18 @@ def humanize_timesince(date):
         return ungettext(u"%d year ago", u"%d years ago", num_years) % num_years
 
     num_weeks = delta.days / 7
-    if (num_weeks > 0):
+    if (num_weeks > 0): # TODO: "last week" if num_weeks == 1
         return ungettext(u"%d week ago", u"%d weeks ago", num_weeks) % num_weeks
 
-    if (delta.days > 0):
+    if (delta.days > 0): # TODO: "yesterday" if days == 1
         return ungettext(u"%d day ago", u"%d days ago", delta.days) % delta.days
 
     num_hours = delta.seconds / 3600
-    if (num_hours > 0):
+    if (num_hours > 0): # TODO: "an hour ago" if num_hours == 1
         return ungettext(u"%d hour ago", u"%d hours ago", num_hours) % num_hours
 
     num_minutes = delta.seconds / 60
-    if (num_minutes > 0):
+    if (num_minutes > 0): # TODO: "a minute ago" if num_minutes == 1
         return ungettext(u"%d minute ago", u"%d minutes ago", num_minutes) % num_minutes
 
     return ugettext(u"just now")
