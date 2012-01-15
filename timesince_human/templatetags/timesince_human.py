5d0f613e8e535093ea1ca1bbd0f3c5460fcaf22d
Markus Kaiserswerth <github@sensun.org>
2012-01-15 18:36:44 +0100

    Various changes (split from a Git subtree, but lost history)

--- timesince_human/templatetags/timesince_human.py
+++ timesince_human/templatetags/timesince_human.py
@@ -4,8 +4,9 @@ import datetime
 
 register = template.Library()
 
+
 @register.filter(name='timesince_human')
-def humanize_timesince(date): # TODO: let user specify format strings
+def humanize_timesince(date):  # TODO: let user specify format strings
     """
     Returns a translated, humanized representation of the time delta between
     a given date and the current date.
@@ -17,21 +18,26 @@ def humanize_timesince(date): # TODO: let user specify format strings
 
     num_years = delta.days / 365
     if (num_years > 0):
-        return ungettext(u"%d year ago", u"%d years ago", num_years) % num_years
+        return ungettext(u"%d year ago", u"%d years ago", num_years) % (
+            num_years,)
 
     num_weeks = delta.days / 7
-    if (num_weeks > 0): # TODO: "last week" if num_weeks == 1
-        return ungettext(u"%d week ago", u"%d weeks ago", num_weeks) % num_weeks
+    if (num_weeks > 0):  # TODO: "last week" if num_weeks == 1
+        return ungettext(u"%d week ago", u"%d weeks ago", 
+            num_weeks) % num_weeks
 
-    if (delta.days > 0): # TODO: "yesterday" if days == 1
-        return ungettext(u"%d day ago", u"%d days ago", delta.days) % delta.days
+    if (delta.days > 0):  # TODO: "yesterday" if days == 1
+        return ungettext(u"%d day ago", u"%d days ago", 
+            delta.days) % delta.days
 
     num_hours = delta.seconds / 3600
-    if (num_hours > 0): # TODO: "an hour ago" if num_hours == 1
-        return ungettext(u"%d hour ago", u"%d hours ago", num_hours) % num_hours
+    if (num_hours > 0):  # TODO: "an hour ago" if num_hours == 1
+        return ungettext(u"%d hour ago", u"%d hours ago", 
+            num_hours) % num_hours
 
     num_minutes = delta.seconds / 60
-    if (num_minutes > 0): # TODO: "a minute ago" if num_minutes == 1
-        return ungettext(u"%d minute ago", u"%d minutes ago", num_minutes) % num_minutes
+    if (num_minutes > 0):  # TODO: "a minute ago" if num_minutes == 1
+        return ungettext(u"%d minute ago", u"%d minutes ago", 
+            num_minutes) % num_minutes
 
     return ugettext(u"just now")
